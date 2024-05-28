06f45ae750837815b49e37bf152032e9d3deaab6
Kosuke Kiuchi <kiuchi.k.ad@m.titech.ac.jp>
2024-05-28 11:43:04 +0900

    多少実装

--- example/tutorial/dynasoar/tutorial.cu
+++ example/tutorial/dynasoar/tutorial.cu
@@ -73,34 +73,72 @@ int main(int argc, char** argv)
   // Now print some stuff.
   allocator_handle->parallel_do<Bar, &Bar::print_second>();
 
-  do_calc << <1, 1 >> > (10);
+  int result;
+  bool isCalculated = false;
+
+  do_calc << <1, 1 >> > (4, &result, &isCalculated);
   cudaDeviceSynchronize();
 
-  allocator_handle->parallel_do<Fib, &Fib::calc>();
-  allocator_handle->parallel_do<Fib, &Fib::calc>();
-  allocator_handle->parallel_do<Fib, &Fib::print_n>();
+  for (int i = 1; i < 7; i++)
+  {
+    if (isCalculated) {
+      printf("-- Result: %i --\n", result);
+      break;
+    }
+    printf("====== Iteration: %i ======\n", i);
+    allocator_handle->parallel_do<Fib, &Fib::calc>();
+    allocator_handle->parallel_do<Fib, &Fib::printInfo>();
+    allocator_handle->parallel_do<Sum, &Sum::calc>();
+    allocator_handle->parallel_do<Sum, &Sum::printInfo>();
+  }
 }
 
-__global__ void do_calc(int n)
+__global__ void do_calc(int n, int* result, bool* isCalculated)
 {
-  new(device_allocator) Fib(n);
+  new(device_allocator) Fib(result, isCalculated, n);
 }
 
 __device__ void Fib::calc()
 {
   if (n <= 1) {
+    if (result) *result = n;
+    if (isCalculated) *isCalculated = true;
     destroy(device_allocator, this);
     return;
   }
-  Sum* sum = new(device_allocator) Sum();
-  new(device_allocator) Fib(n - 1);
-  new(device_allocator) Fib(n - 2);
+  Sum* sum = new(device_allocator) Sum(result, isCalculated);
+  new(device_allocator) Fib(&sum->x, &sum->isXCalculated, n - 1);
+  new(device_allocator) Fib(&sum->y, &sum->isYCalculated, n - 2);
+  destroy(device_allocator, this);
+}
+
+__device__ void Fib::printInfo()
+{
+  printf("N: %i\n", (int)n);
 }
 
 __device__ void Sum::calc()
 {
-  if (counter == 0)
+  if (isXCalculated && isYCalculated)
   {
-    int result = x + y;
+    *result = x + y;
+    *isCalculated = true;
+    destroy(device_allocator, this);
+  }
+}
+
+__device__ void Sum::printInfo()
+{
+  if (isXCalculated && isYCalculated)
+  {
+    printf("X: %i, Y: %i\n", (int)x, (int)y);
+  }
+  else if (isXCalculated)
+  {
+    printf("X: %i\n", (int)x);
+  }
+  else if (isYCalculated)
+  {
+    printf("Y: %i\n", (int)y);
   }
 }
--- example/tutorial/dynasoar/tutorial.h
+++ example/tutorial/dynasoar/tutorial.h
@@ -9,38 +9,45 @@ class Sum;
 
 using AllocatorT = SoaAllocator</*num_objs=*/ 262144, Foo, Bar, Fib, Sum>;
 
-__global__ void do_calc(int n);
+__global__ void do_calc(int n, int* result, bool* isCalculated);
 
 class Fib : public AllocatorT::Base
 {
 public:
-  declare_field_types(Fib, int)
+  declare_field_types(Fib, int*, bool*, int)
 
 private:
-  Field<Fib, 0> n;
+  Field<Fib, 0> result;
+  Field<Fib, 1> isCalculated;
+  Field<Fib, 2> n;
 
 public:
-  __device__ Fib(int n) : n(n) {}
+  __device__ Fib(int* result, bool* isCalculated, int n)
+    : result(result), isCalculated(isCalculated), n(n) {}
 
   __device__ void calc();
 
-  __device__ void print_n() {
-    printf("N: %i\n", (int)n);
-  }
+  __device__ void printInfo();
 };
 
 class Sum : public AllocatorT::Base
 {
 public:
-  declare_field_types(Sum, int, int, int)
-
-private:
-  Field<Sum, 0> x;
-  Field<Sum, 1> y;
-  Field<Sum, 1> counter;
+  declare_field_types(Sum, int*, bool*, int, int, bool, bool)
 
 public:
-  __device__ Sum() {}
+  Field<Sum, 0> result;
+  Field<Sum, 1> isCalculated;
+  Field<Sum, 2> x;
+  Field<Sum, 3> y;
+  Field<Sum, 4> isXCalculated;
+  Field<Sum, 5> isYCalculated;
+
+public:
+  __device__ Sum(int* result, bool* isCalculated)
+    : result(result), isCalculated(isCalculated) {}
 
   __device__ void calc();
+
+  __device__ void printInfo();
 };
